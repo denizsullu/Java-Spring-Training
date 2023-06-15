161e29c2383e95119ce219846c5a2db4a0e920f5
deniz <denizasus40@gmail.com>
2023-06-15 20:59:03 +0300

    java

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        System.out.println("deneme");
+        String arrow = "deniz hoşgeldin";
+        System.out.println(arrow);
+    }
+}
