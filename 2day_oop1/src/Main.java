e7bd1148f5493fbf19fae39c8ad6562a937f3d02
deniz <denizasus40@gmail.com>
2023-06-17 20:29:43 +0300

    day2

---
+++ 2day_oop1/src/Main.java
@@ -0,0 +1,42 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Product product1 = new Product();
+		product1.setName("Delongi KAhve Makinesi");
+		product1.setDiscount(7);
+		product1.setUnitPrice(7500);
+		product1.setUnitsInStock(3);
+		product1.setImageUrl("image1.jpg");
+		
+		Product product2 = new Product();
+		product2.setName("Smeg KAhve Makinesi");
+		product2.setDiscount(7);
+		product2.setUnitPrice(7500);
+		product2.setUnitsInStock(3);
+		product2.setImageUrl("image2.jpg");
+
+		Product product3 = new Product();
+		product3.setName("Kitchen Aid KAhve Makinesi");
+		product3.setDiscount(7);
+		product3.setUnitPrice(7500);
+		product3.setUnitsInStock(3);
+		product3.setImageUrl("image3.jpg");
+
+		Product[] products = { product1, product2, product3 };
+
+		for (Product product : products) {
+			System.out.println(product.getName());
+		}
+		
+		IndividualCustomer individualCustomer = new IndividualCustomer();
+		individualCustomer.setId(1);
+		individualCustomer.setPhone("05418503333");
+		individualCustomer.setCustomerNumber("12345");
+		individualCustomer.setFirstName("deniz");
+		individualCustomer.setLastName("süllü");
+		
+	}
+
+}
